9340f2082e1882e721e69264635743edffca0866
fayyazkarim <derofayyaz61@gmail.com>
2021-01-12 04:24:01 +0500

    changs

--- aswo/src/App.js
+++ aswo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h2>Test</h2>
+     <h2 className="text-dark mb-5">Test</h2>
     </div>
   );
 }
